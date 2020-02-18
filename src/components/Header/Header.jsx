7531f2b88571192eae80c602552cd22f52166f8a
romservik29 <andrei.romservik29@gmail.com>
2020-02-18 21:00:19 +0300

    authorization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import classes from './Header.module.css';
+import img from '../../img/YinYan.jpg'
+import svg from '../../img/oval.svg'
+import { NavLink } from 'react-router-dom';
 
 
-const Header = () =>{
-    return(
-        <header className={classes.header}>
-        <img src="https://dynamic.brandcrowd.com/asset/logo/62fdfa68-3a48-46da-9959-be64842d7c47/logo?v=4" alt=""/>
-      </header>
-    )
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <img src="https://dynamic.brandcrowd.com/asset/logo/62fdfa68-3a48-46da-9959-be64842d7c47/logo?v=4" alt="" />
+      <div  className={classes.userLogin}>
+        <NavLink to="/login">
+          <img
+            src={props.isAuth
+              ? img
+              : svg}
+            alt="" />
+        </NavLink>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers } from "redux";
 import dialogsReducer from './dialogs-reducer';
 import profileReducer from './profile-reducer';
 import novelsReducer from "./novels-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    novelsPage: novelsReducer
+    novelsPage: novelsReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
